6d6939fe6b762f0e1305933637ca91a75b14be2b
Monu Kumar <mkmiglani088@gmail.com>
2026-01-20 16:30:39 +0530

    FEW CHANGES

--- fin.py
+++ fin.py
@@ -7,17 +7,17 @@ import matplotlib.pyplot as plt
 
 st.header("Stock Price App")
 
-input_ticker = st.text_input("Enter Stock Ticker")
+input_ticker = st.text_input("Enter Stock Ticker", value="AAPL")
 
 ticker_data= yf.Ticker(input_ticker) #Ticker is a class in yfinance and ticker_data is object of that class
 
 col1, col2 = st.columns(2)
 
 with col1:
-    start_dt = st.date_input("Start Date")
+    start_dt = st.date_input("Start Date", value=pd.to_datetime("2020-01-01"))
 
 with col2:
-    end_dt = st.date_input("End Date")
+    end_dt = st.date_input("End Date", value=pd.to_datetime("2026-01-01"))
 
 ticker_df = ticker_data.history(start=start_dt,
                                 end=end_dt) #here we can specify the period we want to analyze
